bfc2a15e99aab15c70b6f8ffa519063e17eae2ea
AbyssWalker1990 <bordiian.v@gmail.com>
2026-04-11 15:47:30 +0300

    add stack constructor and a few basic methods

---
+++ stack/constructor.js
@@ -0,0 +1,18 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor(value) {
+    const newNode = new Node(value);
+    this.top = newNode;
+    this.length = 1;
+  }
+}
+
+const myStack = new Stack(22);
+
+console.log(myStack);
---
+++ stack/push.js
@@ -0,0 +1,34 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor(value) {
+    const newNode = new Node(value);
+    this.top = newNode;
+    this.length = 1;
+  }
+
+  push(value) {
+    const newNode = new Node(value);
+
+    if (this.length === 0) {
+      this.top = newNode;
+    } else {
+      newNode.next = this.top;
+      this.top = newNode;
+    }
+
+    this.length++;
+
+    return this;
+  }
+}
+
+const myStack = new Stack(22);
+myStack.push(1);
+myStack.push(2);
+myStack.push(3);
